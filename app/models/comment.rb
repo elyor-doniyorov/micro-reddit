4eeee992e194b041af88d9ff214102bac717b28d
elyor doniyorov <doniyorovelyor@gmail.com>
2021-05-08 19:23:51 +0500

    Fix rubocop offenses

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
-    belongs_to :user
-    belongs_to :post
+  belongs_to :user
+  belongs_to :post
 
-    validates :body, presence:true
+  validates :body, presence: true
 end
